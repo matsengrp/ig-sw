9a4ee436b1640c9b446be95f1063ab01fa93abf4
Annak17 <annak17@lakesideschool.org>
2016-06-21 10:16:16 -0700

    Added close file

--- src/readseq.cpp
+++ src/readseq.cpp
@@ -33,6 +33,7 @@ extern "C" void ReadFile() {
   // printf("return value: %d\n", l);
   kseq_destroy(seq); // STEP 5: destroy seq
   gzclose(fp);       // STEP 6: close the file handler
+  fclose(writefp);
 }
 
 std::vector<std::string> OpenFile(std::string filename, bool label) {
@@ -92,8 +93,7 @@ std::vector<std::string> OpenFile(std::string filename, bool label) {
 // }
 
 TEST_CASE("Comparison") {
-  // ReadFile();
-  // sleep(5);
+  ReadFile();
   std::vector<std::string> parsed_vector = OpenFile("readtest.txt", true);
   std::vector<std::string> original_vector = OpenFile("test.fastq", false);
   REQUIRE(original_vector.size() == parsed_vector.size());
